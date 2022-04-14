9505820139d4487f2dd05b79da0938e071ed5d0d
Павел Куракин <bobik>
2022-04-14 20:17:51 +0300

    code: added svg output

--- main.cpp
+++ main.cpp
@@ -70,6 +70,49 @@ void show_histogram_text (vector<size_t> bins) {
         cout << '\n';
     }}
 
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='"  <<baseline<<   "'>" <<text << "</text>";
+}
+
+void svg_rect(double x, double y, double width, double height) {
+cout << "<rect x='"<< x <<"' y='"<<y <<"' width='"<<width<<"' height='"<<height<<"' />";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    top += BIN_HEIGHT;
+}
+    svg_end();
+}
+
+
+
 int main() {
     // ¬вод данных
     size_t number_count;
@@ -93,7 +136,8 @@ const auto numbers = input_numbers(number_count);   //‘ункиц€ зaполнени€ массива
 
     // ¬ывод данных
 
-    show_histogram_text(bins);
+   /* show_histogram_text(bins); */
+   show_histogram_svg (bins);
 
 
     return 0;
