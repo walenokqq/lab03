cd7031b76720a09a5fb632704aef572c9992399a
Павел Куракин <bobik>
2022-10-05 23:52:05 +0300

    code: загрузка файла из сети

--- main.cpp
+++ main.cpp
@@ -50,8 +50,27 @@ vector <size_t> make_histogram (struct Input data) {
     return bins;
 }
 
-int main() {
-   curl_global_init(CURL_GLOBAL_ALL);
+int main(int argc, char* argv[]) {
+    curl_global_init(CURL_GLOBAL_ALL);
+    if (argc>1)
+    {
+       CURL* curl = curl_easy_init();
+       if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+
+     const auto input = read_input(cin,true);
+
+
+     const auto bins = make_histogram(input);
+
+    show_histogram_svg(bins);
+    return 0;
+        }
+        return 0;
+    }
    const auto input = read_input(cin, true);
 
     // Обработка данных
