ba2b15b7980fa3a22662807cbd3da90bd29921be
Павел Куракин <bobik>
2022-10-13 01:37:05 +0300

    code: добавлена write_data для работы с примером

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include "histogram.h"
 #include "svg.h"
 #include <curl/curl.h>
-
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -49,25 +50,43 @@ vector <size_t> make_histogram (struct Input data) {
     return bins;
 }
 
-int main(int argc, char* argv[]) {
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    auto data_size=item_size*item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<char*>(items), data_size);
+    return data_size;
+}
+
+Input
+    download(const string& address) {
+    stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
-    if (argc>1)
-    {
-       CURL* curl = curl_easy_init();
+    CURL* curl = curl_easy_init();
+
        if(curl) {
             CURLcode res;
-                (curl, CURLOPT_URL, argv[1]);
+            curl_easy_setopt(curl, CURLOPT_URL,  address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
             res = curl_easy_perform(curl);
-            if (res != CURLE_OK)
-            {
+            if (res)            {
                 cerr << curl_easy_strerror(res) << endl;
                 exit(1);
             }
-            curl_easy_cleanup(curl);
         }
-        return 0;
+                curl_easy_cleanup(curl);
+    return read_input(buffer, false);
+}
+
+
+int main(int argc, char* argv[]){
+   Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
     }
-   const auto input = read_input(cin, true);
+     else {
+        input = read_input(cin, true);
+    }
 
     // Обработка данных
     const auto bins = make_histogram(input);
