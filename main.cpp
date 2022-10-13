f7ea683b66d9d745816cb767704f9ebf5a13a9de
Павел Куракин <bobik>
2022-10-13 16:27:35 +0300

    Revert "Форматный вывод в стиле C (printf)"
    This reverts commit a5865d38ca57dcfa8f127816276344b2eaf3ae86.

--- main.cpp
+++ main.cpp
@@ -16,11 +16,6 @@ vector <double> input_numbers(istream& in, size_t count) {
 
 int main() {
     // ¬вод данных
-        const char* name = "Commander Shepard";
-    int year = 2154;
-    printf("%s was born in %d.\n", name, year);
-    printf("n = %08x\n", 0x1234567);
-    return 0;
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
